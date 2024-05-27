b69d7ca7200a43f763162e028a7ef2a8047132a6
Shadrak Lagadi <shadrl19@gmail.com>
2024-05-27 12:42:54 -0600

    Add user authentication routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
   root to: 'products#index'
+
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
